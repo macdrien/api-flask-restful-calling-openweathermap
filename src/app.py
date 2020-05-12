3572cba0d79962a836742602512ba6539277bf8b
macdrien <adrien.bouyssou@gmail.com>
2020-05-12 16:32:37 +0200

    [ADD] Add /forecast api

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
 
 from src.resources.Weather import Weather
+from src.resources.Forecast import Forecast
 
 app = Flask(__name__)
 api = Api(app)
 
 # Routes
 api.add_resource(Weather, '/weather', '/weather/<string:cityName>')
+api.add_resource(Forecast, '/forecast', '/forecast/<string:cityName>')
 
 if __name__ == '__main__':
     app.run(debug=True)
